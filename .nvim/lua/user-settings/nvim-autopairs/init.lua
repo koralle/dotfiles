9368fdb798b195f802732943759f56db0453b044
koralle <mkhd7930@gmail.com>
2022-04-30 04:42:04 +0900

    wip

--- .nvim/lua/user-settings/nvim-autopairs/init.lua
+++ .nvim/lua/user-settings/nvim-autopairs/init.lua
@@ -1,1 +1,6 @@
+local cmp_autopairs = require('nvim-autopairs.completion.cmp')
+local cmp = require('cmp')
+
 require('nvim-autopairs').setup {}
+
+cmp.event:on('comfirm_done', cmp_autopairs.on_confirm_done({ map_char = { tex = '' } }))
