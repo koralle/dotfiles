8ad1333a548baeedba811c64b24dc95ad1620bb1
koralle <mkhd7930@gmail.com>
2022-05-11 09:03:59 +0900

    tiny fix

--- .nvim/lua/user-settings/dap/init.lua
+++ .nvim/lua/user-settings/dap/init.lua
@@ -1,19 +1,21 @@
 local dap = require('dap')
+local cmd = vim.env.HOME .. '/.vscode/extensions/vadimcn.vscode-lldb-1.7.0/adapter/codelldb'
 
 require('telescope').load_extension('dap')
 
-local opts = { silent = true, noremap = true }
-vim.api.nvim_set_keymap('n', '<F5>', ":lua require'dap'.continue()<CR>", opts)
-vim.api.nvim_set_keymap('n', '<F10>', ":lua require'dap'.step_over()<CR>", opts)
-vim.api.nvim_set_keymap('n', '<F11>', ":lua require'dap'.step_into()<CR>", opts)
-vim.api.nvim_set_keymap('n', '<F12>', ":lua require'dap'.step_out()<CR>", opts)
-vim.api.nvim_set_keymap('n', '<Leader>b', ":lua require'dap'.toggle_breakpoint()<CR>", opts)
-vim.api.nvim_set_keymap('n', '<Leader>B', ":lua require'dap'.set_breakpoint(vim.fn.input('Breakpoint condition: '))<CR>", opts)
-vim.api.nvim_set_keymap('n', '<Leader>lp', ":lua require'dap'.set_breakpoint(nil, nil, vim.fn.input('Log point message '))<CR>", opts)
-vim.api.nvim_set_keymap('n', '<Leader>dr', ":lua require'dap'.repl.open()<CR>", opts)
-vim.api.nvim_set_keymap('n', '<Leader>dl', ":lua require'dap'.run_last()<CR>", opts)
+local options = { silent = true, noremap = true }
+vim.api.nvim_set_keymap('n', '<F5>', ":lua require'dap'.continue()<CR>", options)
+vim.api.nvim_set_keymap('n', '<F10>', ":lua require'dap'.step_over()<CR>", options)
+vim.api.nvim_set_keymap('n', '<F11>', ":lua require'dap'.step_into()<CR>", options)
+vim.api.nvim_set_keymap('n', '<F12>', ":lua require'dap'.step_out()<CR>", options)
+vim.api.nvim_set_keymap('n', '<Leader>b', ":lua require'dap'.toggle_breakpoint()<CR>", options)
+vim.api.nvim_set_keymap('n', '<Leader>B', ":lua require'dap'.set_breakpoint(vim.fn.input('Breakpoint condition: '))<CR>", options)
+vim.api.nvim_set_keymap('n', '<Leader>lp', ":lua require'dap'.set_breakpoint(nil, nil, vim.fn.input('Log point message '))<CR>", options)
+vim.api.nvim_set_keymap('n', '<Leader>dr', ":lua require'dap'.repl.open()<CR>", options)
+vim.api.nvim_set_keymap('n', '<Leader>dl', ":lua require'dap'.run_last()<CR>", options)
 
-local cmd = vim.env.HOME .. '/.vscode/extensions/vadimcn.vscode-lldb-1.7.0/adapter/codelldb'
+vim.fn.sign_define('DapBreakpoint', { text='⛔', texthl='', linehl='', numhl=''})
+vim.fn.sign_define('DapStopped', { text='👉', texthl='', linehl='', numhl=''})
 
 dap.adapters.codelldb = function(on_adapter)
   -- This asks the system for a free port
