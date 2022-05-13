a148f02e236a1699575734467b0fdd35b41cb2d9
koralle <mamoru.kosugiyama@ncdc.co.jp>
2022-05-13 17:00:15 +0900

    nvim-tree.luaのcustom keybindingsを定義

--- .nvim/lua/user-settings/nvim-tree_lua/init.lua
+++ .nvim/lua/user-settings/nvim-tree_lua/init.lua
@@ -2,11 +2,27 @@ vim.api.nvim_set_keymap('n', '<C-n>', ':NvimTreeToggle<CR>', { noremap = true })
 vim.api.nvim_set_keymap('n', '<leader>r', ':NvimTreeRefresh<CR>', { noremap = true })
 vim.api.nvim_set_keymap('n', '<leader>', ':NvimTreeFindFile<CR>', { noremap = true })
 
+local list = {
+  -- 'simeji/winresizer'のkeybindingsと被るため
+  { key = "s", action = "" },
+  { key = "<C-e>", action = "" },
+
+  -- 単純に好みのカスタマイズ
+  { key = {"<CR>", "o", "<2-LeftMouse>"}, action = "cd" },
+  { key = "l", action = "edit" },
+  { key = "-", action = "" },
+  { key = "<BS>", action = "dir_up" },
+}
+
 require'nvim-tree'.setup {
   view = {
     hide_root_folder = true,
     width = 40,
-    side = "right"
+    side = "right",
+    mappings = {
+      custom_only = false,
+      list = list
+    }
   }
 }
 
