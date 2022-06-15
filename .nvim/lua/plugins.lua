7be76cc292dbdc11dc7f662e5c73350b8c1b3098
koralle <mkhd7930@gmail.com>
2022-06-15 21:01:12 +0900

    add Plugin: nvim-treesitter/nvim-treesitter-context

--- .nvim/lua/plugins.lua
+++ .nvim/lua/plugins.lua
@@ -72,6 +72,10 @@ require("packer").startup(function(use)
     "nvim-treesitter/nvim-treesitter",
     run = ":TSUpdate",
   })
+  use({
+    "nvim-treesitter/nvim-treesitter-context",
+    requires = "nvim-treesitter/nvim-treesitter",
+  })
 
   use("yuttie/comfortable-motion.vim")
   use({
