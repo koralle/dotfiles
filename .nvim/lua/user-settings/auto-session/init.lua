0af540820b7e96e3cad794ae897dbb1416b30461
koralle <mkhd7930@gmail.com>
2022-08-19 09:49:03 +0900

    update log_level for auto-session: info to error

--- .nvim/lua/user-settings/auto-session/init.lua
+++ .nvim/lua/user-settings/auto-session/init.lua
@@ -9,7 +9,7 @@ end
 vim.o.sessionoptions = "blank,buffers,curdir,folds,help,help,tabpages,winsize,winpos,terminal"
 session.setup({
   -- Sets the log level of the plugin
-  log_level = "info",
+  log_level = "error",
   -- Enables/disables auto saving
   auto_save_enabeld = true,
   -- Use the git branch to differentiate the session name
