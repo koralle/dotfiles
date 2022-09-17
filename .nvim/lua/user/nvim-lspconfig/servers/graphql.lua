4320e678af7e5ceffc76bb97c8f1e3751a8e582a
koralle <mkhd7930@gmail.com>
2022-09-17 16:39:46 +0900

    setup config for LSP of GraphQL

---
+++ .nvim/lua/user/nvim-lspconfig/servers/graphql.lua
@@ -0,0 +1,43 @@
+local graphql = {}
+
+graphql.setup = function(nvim_lsp)
+  local my_capabilities_status, my_capabilities = pcall(require, "user.nvim-lspconfig.capabilities")
+  if not my_capabilities_status then
+    return
+  end
+
+  local my_utils_status, my_utils = pcall(require, "user.nvim-lspconfig.utils")
+  if not my_utils_status then
+    return
+  end
+
+  local my_highlight_status, my_highlight = pcall(require, "user.nvim-lspconfig.highlight")
+  if not my_highlight_status then
+    return
+  end
+
+  local mason_lspconfig_status, mason_lspconfig = pcall(require, "mason-lspconfig")
+  if not mason_lspconfig_status then
+    return
+  end
+
+  mason_lspconfig.setup_handlers({
+    function()
+      nvim_lsp.graphql.setup({
+        capabilities = my_capabilities.capabilities,
+        on_attach = function(client, buffer_number)
+          my_utils.disable_formatting_via_lspconfig(client, buffer_number)
+          my_highlight.setup(client, buffer_number)
+        end,
+        filetypes = {
+          "graphqls", -- use on 99designs/gqlgen
+          "graphql",
+          "javascriptreact",
+          "typescriptreact",
+        },
+      })
+    end,
+  })
+end
+
+return graphql
