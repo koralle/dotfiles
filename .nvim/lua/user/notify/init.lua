c80f95ead10251463bdbef7634ba4b8abc1a42e9
koralle <mkhd7930@gmail.com>
2022-12-22 00:58:29 +0900

    非推奨になったhighlight用の関数を新しい関数に変更
    1. vim.highlight.create
    2. vim.highlight.link
    
    vim.api.nvim_set_hl

--- .nvim/lua/user/notify/init.lua
+++ .nvim/lua/user/notify/init.lua
@@ -7,35 +7,34 @@ notify.setup({
   background_colour = "#3b4252",
 })
 
-local create_hl = vim.highlight.create
-local link_hl = vim.highlight.link
+local set_hl = vim.api.nvim_set_hl
 
 -- Highlight Error
-create_hl("NotifyERRORBorder", { guifg = "#d84f76" })
-create_hl("NotifyERRORIcon", { guifg = "#d84f76" })
-create_hl("NotifyERRORTitle", { guifg = "#d84f76" })
-link_hl("NotifyErrorBody", "Normal")
+set_hl(0, "NotifyERRORBorder", { fg = "#d84f76" })
+set_hl(0, "NotifyERRORIcon", { fg = "#d84f76" })
+set_hl(0, "NotifyERRORTitle", { fg = "#d84f76" })
+set_hl(0, "NotifyErrorBody", { link = "Normal" })
 
 -- Highlight Warn
-create_hl("NotifyWARNBorder", { guifg = "#f6c177" })
-create_hl("NotifyWARNIcon", { guifg = "#f6c177" })
-create_hl("NotifyWARNTitle", { guifg = "#f6c177" })
-link_hl("NotifyWARNBody", "Normal")
+set_hl(0, "NotifyWARNBorder", { fg = "#f6c177" })
+set_hl(0, "NotifyWARNIcon", { fg = "#f6c177" })
+set_hl(0, "NotifyWARNTitle", { fg = "#f6c177" })
+set_hl(0, "NotifyWARNBody", { link = "Normal" })
 
 -- Highlight Info
-create_hl("NotifyINFOBorder", { guifg = "#569fba" })
-create_hl("NotifyINFOIcon", { guifg = "#569fba" })
-create_hl("NotifyINFOTitle", { guifg = "#569fba" })
-link_hl("NotifyInfoBody", "Normal")
+set_hl(0, "NotifyINFOBorder", { fg = "#569fba" })
+set_hl(0, "NotifyINFOIcon", { fg = "#569fba" })
+set_hl(0, "NotifyINFOTitle", { fg = "#569fba" })
+set_hl(0, "NotifyInfoBody", { link = "Normal" })
 
 -- Highlight Debug
-create_hl("NotifyDEBUGBorder", { guifg = "#60728a" })
-create_hl("NotifyDEBUGIcon", { guifg = "#60728a" })
-create_hl("NotifyDEBUGTitle", { guifg = "#60728a" })
-link_hl("NotifyDEBUGBody", "Normal")
+set_hl(0, "NotifyDEBUGBorder", { fg = "#60728a" })
+set_hl(0, "NotifyDEBUGIcon", { fg = "#60728a" })
+set_hl(0, "NotifyDEBUGTitle", { fg = "#60728a" })
+set_hl(0, "NotifyDEBUGBody", { link = "Normal" })
 
 -- Highlight Trace
-create_hl("NotifyTRACEBorder", { guifg = "#c4a7e7" })
-create_hl("NotifyTRACEIcon", { guifg = "#c4a7e7" })
-create_hl("NotifyTRACETitle", { guifg = "#c4a7e7" })
-link_hl("NotifyTRACEBody", "Normal")
+set_hl(0, "NotifyTRACEBorder", { fg = "#c4a7e7" })
+set_hl(0, "NotifyTRACEIcon", { fg = "#c4a7e7" })
+set_hl(0, "NotifyTRACETitle", { fg = "#c4a7e7" })
+set_hl(0, "NotifyTRACEBody", { link = "Normal" })
