6764970d128d5c1e845ca919402e90630c1d8d75
koralle <mkhd7930@gmail.com>
2023-02-19 23:13:57 +0900

    add: kyazdani42/nvim-tree.lua

--- .nvim/lua/plugins.lua
+++ .nvim/lua/plugins.lua
@@ -16,11 +16,84 @@ end
 vim.opt.runtimepath:prepend(lazypath)
 
 require("lazy").setup({
-  { 
+  {
     "folke/tokyonight.nvim",
     branch = "main",
     config = function()
       vim.cmd([[colorscheme tokyonight-storm]])
     end
+  },
+  { "hrsh7th/cmp-nvim-lsp" },
+  {
+    "nvim-tree/nvim-web-devicons",
+    lazy = true,
+  },
+  {
+    "windwp/nvim-autopairs",
+    lazy = true,
+    config = function()
+      require("user.autopairs")
+    end,
+  },
+  {
+    "kyazdani42/nvim-tree.lua",
+    dependencies = {
+      "nvim-tree/nvim-web-devicons",
+    },
+    cmd = {
+      "NvimTreeOpen",
+      "NvimTreeClose",
+      "NvimTreeToggle",
+      "NvimTreeFocus",
+      "NvimTreeRefresh",
+      "NvimTreeFindFile",
+      "NvimTreeFindFileToggle",
+      "NvimTreeClipboard",
+      "NvimTreeResize",
+      "NvimTreeCollapse",
+      "NvimTreeCollapseKeepBuffers",
+    },
+    keys = {
+      {
+        "<C-n>",
+        "<cmd>NvimTreeToggle<cr>",
+        noremap = true,
+        mode = "n",
+      },
+    },
+    config = function()
+      require("user.nvim-tree_lua")
+    end,
+  },
+  {
+    "williamboman/mason.nvim",
+    cmd = { "Mason" },
+    config = function()
+      local mason = require("mason")
+      mason.setup({
+        ui = {
+          icons = {
+            package_installed = "✓",
+            package_pending = "➜",
+            package_uninstalled = "✗",
+          },
+        },
+      })
+    end,
+  },
+  {
+    "neovim/nvim-lspconfig",
+    lazy = false,
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    dependencies = {
+      "williamboman/mason.nvim",
+      "neovim/nvim-lspconfig",
+      "hrsh7th/cmp-nvim-lsp",
+    },
+    config = function()
+      require("user.nvim-lspconfig")
+    end,
   },
 })
