46c2bbb2ad73cacb66f9cb9379a5f708a8132a9d
koralle <mkhd7930@gmail.com>
2023-02-20 00:44:17 +0900

    fix: init.lua

--- .nvim/init.lua
+++ .nvim/init.lua
@@ -1,6 +1,8 @@
 -- load plugins
 require("plugins")
 
+vim.o.termguicolors = true
+
 local set_keymap_for_window = function(opts)
   local keymap_set = vim.keymap.set
   keymap_set("n", "<space>s", ":<C-u>sp<CR><C-w>j", opts)
@@ -19,3 +21,42 @@ vim.bo.expandtab = true
 vim.bo.shiftwidth = 2
 vim.bo.softtabstop = 2
 vim.bo.tabstop = 2
+
+--行番号を表示する
+vim.wo.number = true
+
+--現在いる列をハイライトする
+vim.wo.cursorcolumn = true
+
+--現在いる行をハイライトする
+vim.wo.cursorline = true
+
+--自動インデント
+vim.bo.smartindent = true
+
+-- ファイル編集中にバッファの切り替えが可能
+vim.o.hidden = true
+
+-- フォントの設定
+local font = "Cica 20"
+vim.o.guifont = font
+vim.o.guifontwide = font
+
+-- ターミナルモード時のkeymap
+vim.keymap.set("t", "<Esc>", "<C-\\><C-n>", { noremap = true })
+vim.keymap.set("t", "<C-[>", "<C-\\><C-n>", { noremap = true })
+
+-- コマンドモードで"T"を入力すると現在のウインドウの下にTerminalを開く
+vim.cmd("command! -nargs=* T split | wincmd j | resize 20 | terminal <args>")
+-- 常にインサートモードでTerminalを開く
+vim.cmd("autocmd TermOpen * startinsert")
+
+vim.o.encoding = "utf-8"
+
+vim.o.cmdheight = 0
+
+vim.o.list = true
+vim.o.listchars = "tab:»-,trail:-,eol:↲,extends:»,precedes:«,nbsp:%,space:_"
+
+vim.keymap.set("n", "+", "<C-a>", { noremap = true, silent = true })
+vim.keymap.set("n", "-", "<C-x>", { noremap = true, silent = true })
