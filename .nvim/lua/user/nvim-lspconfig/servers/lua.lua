be1bd07275fbe8b244160cb03da78e5525ce156a
koralle <mkhd7930@gmail.com>
2023-03-15 21:29:56 +0900

    update: user/lspconfig/servers/lua.lua

--- .nvim/lua/user/nvim-lspconfig/servers/lua.lua
+++ .nvim/lua/user/nvim-lspconfig/servers/lua.lua
@@ -1,19 +1,21 @@
 local M = {}
 
+local ERROR = "ERROR"
+
 M.setup = function(nvim_lsp)
   local my_utils_status, my_utils = pcall(require, "user.nvim-lspconfig.utils")
   if not my_utils_status then
-    vim.notify(my_utils)
+    vim.notify(my_utils, ERROR)
   end
 
   local my_highlight_status, my_highlight = pcall(require, "user.nvim-lspconfig.highlight")
   if not my_highlight_status then
-    vim.notify(my_highlight)
+    vim.notify(my_highlight, ERROR)
   end
 
   local mason_lspconfig_status, mason_lspconfig = pcall(require, "mason-lspconfig")
   if not mason_lspconfig_status then
-    vim.notify(mason_lspconfig)
+    vim.notify(mason_lspconfig, ERROR)
   end
 
   mason_lspconfig.setup_handlers({
