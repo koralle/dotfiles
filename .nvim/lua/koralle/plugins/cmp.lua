3fd201350857125d15635cf0f9c2feaa946787db
koralle <mkhd7930@gmail.com>
2023-03-16 20:58:28 +0900

    chore(lazy.nvim): cmp

---
+++ .nvim/lua/koralle/plugins/cmp.lua
@@ -0,0 +1,172 @@
+local setup_cmp = function()
+  local cmp = require("cmp")
+  local luasnip = require("luasnip")
+  local lspkind = require("lspkind")
+
+  local has_words_before = function()
+    local line = vim.api.nvim_win_get_cursor(0)[1]
+    local col = vim.api.nvim_win_get_cursor(0)[2]
+    return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+  end
+
+  cmp.setup({
+    enabled = true,
+    preselect = cmp.PreselectMode.Item,
+    formatting = {
+      fields = { "kind", "abbr", "menu" },
+      format = function(entry, vim_item)
+        local kind = lspkind.cmp_format({ mode = "symbol_text", maxwidth = 50 })(entry, vim_item)
+        local strings = vim.split(kind.kind, "%s", { trimempty = true })
+        kind.kind = " " .. strings[1] .. " "
+        kind.menu = "    [" .. strings[2] .. "]"
+
+        return kind
+      end,
+    },
+    experimental = {
+      ghost_text = true,
+    },
+    view = {
+      entries = { name = "custom", selection_order = "near_cursor" },
+    },
+    window = {
+      completion = {
+        winhighlight = "Normal:Pmenu,FloatBorder:Pmenu,Search:None",
+        col_offset = -3,
+        side_padding = 0,
+      },
+    },
+    snippet = {
+      expand = function(args)
+        luasnip.lsp_expand(args.body)
+      end,
+    },
+    sources = cmp.config.sources({
+      { name = "nvim_lsp" },
+      { name = "luasnip" },
+      { name = "emoji" },
+      { name = "nvim_lua" },
+      { name = "nvim_lsp_signature_help" },
+      { name = "treesitter" },
+      { name = "path" },
+      --{ name = "omni" },
+      { name = "npm", keyword_length = 4 },
+    }, {
+      { name = "buffer" },
+    }),
+    mapping = {
+      ["<C-d>"] = cmp.mapping.scroll_docs(-4),
+      ["<C-f>"] = cmp.mapping.scroll_docs(4),
+      ["<C-l>"] = cmp.mapping.complete(),
+      ["<CR>"] = cmp.mapping.confirm({
+        select = true,
+      }),
+      ["<Tab>"] = cmp.mapping(function(fallback)
+        if cmp.visible() then
+          cmp.select_next_item()
+        elseif luasnip.expand_or_jumpable() then
+          luasnip.expand_or_jump()
+        elseif has_words_before() then
+          cmp.complete()
+        else
+          fallback()
+        end
+      end, { "i", "s" }),
+      ["<S-Tab>"] = cmp.mapping(function(fallback)
+        if cmp.visible() then
+          cmp.select_prev_item()
+        elseif luasnip.jumpable(-1) then
+          luasnip.jump(-1)
+        else
+          fallback()
+        end
+      end, { "i", "s" }),
+    },
+  })
+  cmp.setup.filetype("gitcommit", {
+    sources = cmp.config.sources({
+      { name = "buffer" },
+    }),
+  })
+
+  cmp.setup.cmdline("/", {
+    mapping = cmp.mapping.preset.cmdline(),
+    sources = {
+      { name = "buffer" },
+    },
+  })
+
+  cmp.setup.cmdline(":", {
+    mapping = cmp.mapping.preset.cmdline(),
+    sources = cmp.config.sources({
+      { name = "path" },
+    }, {
+      name = "cmdline",
+    }),
+  })
+end
+
+local spec = {
+  {
+    "hrsh7th/nvim-cmp",
+    config = function()
+      setup_cmp()
+    end,
+    dependencies = {
+      "hrsh7th/cmp-nvim-lsp",
+      "hrsh7th/cmp-buffer",
+      "saadparwaiz1/cmp_luasnip",
+      "L3MON4D3/LuaSnip",
+      "hrsh7th/cmp-emoji",
+      "hrsh7th/cmp-path",
+      "hrsh7th/cmp-cmdline",
+      "hrsh7th/cmp-nvim-lua",
+      "hrsh7th/cmp-omni",
+      "hrsh7th/cmp-nvim-lsp-signature-help",
+      "onsails/lspkind.nvim",
+      "David-Kunz/cmp-npm",
+    },
+  },
+  {
+    "hrsh7th/cmp-buffer",
+    lazy = true,
+  },
+  {
+    "saadparwaiz1/cmp_luasnip",
+    lazy = true,
+  },
+  {
+    "L3MON4D3/LuaSnip",
+    lazy = true,
+  },
+  {
+    "hrsh7th/cmp-emoji",
+    lazy = true,
+  },
+  {
+    "onsails/lspkind.nvim",
+    lazy = true,
+  },
+  {
+    "hrsh7th/cmp-path",
+    lazy = true,
+  },
+  {
+    "hrsh7th/cmp-cmdline",
+    lazy = true,
+  },
+  {
+    "hrsh7th/cmp-nvim-lua",
+    lazy = true,
+  },
+  {
+    "hrsh7th/cmp-nvim-lsp-signature-help",
+    lazy = true,
+  },
+  {
+    "hrsh7th/cmp-omni",
+    lazy = true,
+  },
+}
+
+return spec
