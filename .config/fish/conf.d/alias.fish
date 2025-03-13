9736253fd1935dfedf8bcfa28b663e45373ec5d8
koralle <mkhd7930@gmail.com>
2025-03-13 17:58:43 +0900

    feat(gitconfig): refine config of Git

--- .config/fish/conf.d/alias.fish
+++ .config/fish/conf.d/alias.fish
@@ -4,3 +4,8 @@ alias dk='docker'
 alias ls='eza'
 alias grep='rg'
 alias cat='bat'
+
+if command -q git
+  abbr -a ggcf --set-cursor "git commit --fixup=%"
+  abbr -a ggcs --set-cursor "git commit --squash=%"
+end
