6fb27b64e161717f7016a4bc884c62d483290c4a
koralle <mkhd7930@gmail.com>
2025-06-13 19:35:31 +0900

    feat: restore Neovim

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -12,10 +12,3 @@ vim.g.loaded_zipPlugin = 1
 vim.g.loaded_zip = 1
 vim.g.loaded_tarPlugin = 1
 vim.g.loaded_tar = 1
-
-require("lazy-nvim")
-
--- Prepend mise shims to PATH
-vim.env.PATH = vim.env.HOME .. "/.local/share/mise/shims:" .. vim.env.PATH
-
-vim.cmd([[runtime! lua/koralle/configs/*.lua]])
