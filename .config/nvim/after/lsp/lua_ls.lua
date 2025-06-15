4f17f31781c7d4c68a3716c0419f3af1bd1e7eca
koralle <mkhd7930@gmail.com>
2025-06-15 12:58:46 +0900

    feat(neovim): disable formatting by `lua_ls`

--- .config/nvim/after/lsp/lua_ls.lua
+++ .config/nvim/after/lsp/lua_ls.lua
@@ -2,12 +2,15 @@ return {
   settings = {
     Lua = {
       diagnostics = {
-        unusedLocalExclude = { '_*' },
-        globals = { 'vim' }
+        unusedLocalExclude = { "_*" },
+        globals = { "vim" },
+      },
+      format = {
+        disable = true,
       },
       workspace = {
-        library = vim.api.nvim_get_runtime_file("", true)
-      }
-    }
-  }
+        library = vim.api.nvim_get_runtime_file("", true),
+      },
+    },
+  },
 }
