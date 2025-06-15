99a09694ce0f1055054e498f816ff1e1ee753f05
koralle <mkhd7930@gmail.com>
2025-06-15 19:30:06 +0900

    feat(neovim): setup `nanozuki/tabby.nvim`

--- .config/nvim/lua/config/init.lua
+++ .config/nvim/lua/config/init.lua
@@ -1,18 +1,23 @@
+-- global variables
+vim.g.mapleader = " "
+
 -- global
 vim.opt.termguicolors = true
 vim.opt.hidden = true
 vim.opt.backup = true
-vim.opt.ambiwidth = 'single'
-vim.opt.encoding = 'utf-8'
-vim.opt.fileencoding = 'utf-8'
-vim.opt.inccommand = 'split'
+vim.opt.ambiwidth = "single"
+vim.opt.encoding = "utf-8"
+vim.opt.fileencoding = "utf-8"
+vim.opt.inccommand = "split"
 vim.opt.splitbelow = true
 vim.opt.splitright = true
-vim.opt.splitkeep = 'cursor'
+vim.opt.splitkeep = "cursor"
 vim.opt.backspace = { "start", "eol", "indent" }
 vim.opt.path:append({ "**" })
 vim.opt.wildignore:append({ "*/node_modules/*" })
 vim.opt.cmdheight = 0
+vim.opt.showtabline = 2
+vim.opt.sessionoptions = "curdir,folds,globals,help,tabpages,terminal,winsize"
 
 -- local to window
 vim.opt.number = true
---
+++ .config/nvim/lua/plugins/tabby.lua
@@ -0,0 +1,12 @@
+---@type LazySpec
+return {
+  "nanozuki/tabby.nvim",
+  dependencies = {
+    "nvim-tree/nvim-web-devicons",
+    "echasnovski/mini.nvim",
+    "echasnovski/mini.icons",
+  },
+  opts = {
+    preset = "active_wins_at_tail",
+  },
+}
