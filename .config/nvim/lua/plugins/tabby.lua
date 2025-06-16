bd9a488bcb0e7d53500d0a50dd627c8d7da1855f
koralle <mkhd7930@gmail.com>
2025-06-17 01:28:12 +0900

    feat(neovim): update theme of `tabby.nvim`

--- .config/nvim/lua/plugins/tabby.lua
+++ .config/nvim/lua/plugins/tabby.lua
@@ -6,7 +6,38 @@ return {
     "echasnovski/mini.nvim",
     "echasnovski/mini.icons",
   },
-  opts = {
-    preset = "active_wins_at_tail",
-  },
+  config = function()
+    require("tabby").setup({
+      preset = "active_wins_at_tail",
+      option = {
+        theme = {
+          fill = {
+            fg = "#c6d0f5",
+            bg = "#303446",
+          },
+          head = {
+            fg = "#a5adce",
+            bg = "#414559",
+          },
+          current_tab = {
+            fg = "#232634",
+            bg = "#e78284",
+            style = "bold",
+          },
+          tab = {
+            fg = "#a5adce",
+            bg = "#51576d",
+          },
+          win = {
+            fg = "#a5adce",
+            bg = "#414559",
+          },
+          tail = {
+            fg = "#a5adce",
+            bg = "#414559",
+          },
+        },
+      },
+    })
+  end,
 }
