b70442cdfba282e5fe2e0b863aae6afef0149d51
koralle <mkhd7930@gmail.com>
2025-06-16 23:22:09 +0900

    feat(wezterm): format `wezterm.lua`

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,16 +1,16 @@
-local wezterm = require('wezterm')
+local wezterm = require("wezterm")
 
 local config = wezterm.config_builder()
 
-config.default_workspace = 'koralle'
+config.default_workspace = "koralle"
 
 config.tab_bar_at_bottom = true
 config.hide_tab_bar_if_only_one_tab = true
 
-config.color_scheme = 'Darcula (base16)'
+config.color_scheme = "Darcula (base16)"
 
 config.font = wezterm.font_with_fallback({
-  'Moralerspace Xenon'
+  "Moralerspace Xenon",
 })
 
 config.font_size = 14.0
@@ -21,7 +21,7 @@ config.window_background_opacity = 0.8
 config.adjust_window_size_when_changing_font_size = false
 
 -- Key mappings
-config.keys = require('key-mappings')
+config.keys = require("key-mappings")
 
 -- Default prog
 --config.default_prog = { os.getenv('HOME') .. '/' .. '.nix-profile/bin/fish' }
@@ -29,14 +29,14 @@ config.keys = require('key-mappings')
 config.background = {
   {
     source = {
-      File = os.getenv('HOME') .. '/' .. '.config/wezterm/images/sleeping-mugicha.png'
+      File = os.getenv("HOME") .. "/" .. ".config/wezterm/images/sleeping-mugicha.png",
     },
     hsb = {
-      brightness = 0.015
+      brightness = 0.015,
     },
-    vertical_align = 'Middle',
-    horizontal_align = 'Center'
-  }
+    vertical_align = "Middle",
+    horizontal_align = "Center",
+  },
 }
 
 return config
