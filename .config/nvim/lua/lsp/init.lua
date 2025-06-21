73a3e6fff879b461d83de3d33bcece3be2fecbcb
koralle <mkhd7930@gmail.com>
2025-06-21 10:50:02 +0900

    feat(neovim/lsp): setup vue-language-server

--- .config/nvim/lua/lsp/init.lua
+++ .config/nvim/lua/lsp/init.lua
@@ -1,3 +1,46 @@
+vim.lsp.config("vtsls", {
+  init_options = {
+    plugins = {
+      {
+        name = "@vue/typescript-plugin",
+        location = vim.env.HOME
+          .. "/.local/share/mise/installs/npm-vue-typescript-plugin/latest/node_modules/@vue/typescript-plugin",
+        languages = { "javascript", "typescript", "vue" },
+      },
+    },
+  },
+  root_markers = {
+    "package.json",
+  },
+  workspace_required = true,
+  typescript = {
+    tsserver = {
+      maxTsServerMemory = 4096,
+    },
+  },
+  file_types = {
+    "javascript",
+    "javascriptreact",
+    "javascript.jsx",
+    "typescript",
+    "typescriptreact",
+    "typescript.tsx",
+    "vue",
+  },
+})
+
+vim.lsp.config("vue_ls", {
+  init_options = {
+    typescript = {
+      tsdk = vim.env.HOME
+        .. "/.local/share/mise/installs/npm-typescript/latest/node_modules/typescript/lib",
+    },
+    vue = {
+      hybridMode = false,
+    },
+  },
+})
+
 local servers = {
   -- Lua
   "lua_ls",
@@ -7,6 +50,9 @@ local servers = {
 
   -- TypeScript (Deno)
   "denols",
+
+  -- Vue
+  "vue_ls",
 }
 
 vim.lsp.config("*", {
