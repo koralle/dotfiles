cb767f5beeeba239678b9b88e241810c324cb13b
koralle <mkhd7930@gmail.com>
2025-06-22 22:01:13 +0900

    feat(neovim): setup `ddu-source-git_branch`

---
+++ .config/nvim/lua/plugins/ddu/source-git-branch.lua
@@ -0,0 +1,54 @@
+---@type LazySpec
+return {
+  "kyoh86/ddu-source-git_branch",
+  name = "ddu-source_git_branch",
+  dependencies = {
+    "ddu.vim",
+  },
+  init = function()
+    vim.keymap.set(
+      "n",
+      "<space>fb",
+      function()
+        vim.fn["ddu#start"]({
+          name = "ff:git_branch",
+          ui = "ff",
+        })
+      end,
+      {
+        silent = true,
+      }
+    )
+  end,
+  config = function()
+    local helpers = require("helpers.ddu")
+
+    helpers.patch_local("ff:git_branch", {
+      sources = {
+        {
+          name = "git_branch",
+          columns = {
+            "git_branch_head",
+            "git_branch_remote",
+            "git_branch_name",
+            "git_branch_upstream",
+            "git_branch_author",
+          },
+        },
+      },
+      sourceOptions = {
+        ["git_branch"] = {
+          volatile = true,
+          matchers = {
+            "matcher_fzf",
+          },
+        },
+      },
+      kindOptions = {
+        ["git_branch"] = {
+          defaultAction = "switch",
+        },
+      },
+    })
+  end,
+}
