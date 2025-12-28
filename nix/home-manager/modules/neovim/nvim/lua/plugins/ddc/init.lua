dcd0d1e95982c0588d5669edd247ba7b345f4ebf
koralle <koralle+github@koralle-mgmg.com>
2025-12-28 23:06:57 +0900

    feat(neovim): refine config of ddc.vim

--- nix/home-manager/modules/neovim/nvim/lua/plugins/ddc/init.lua
+++ nix/home-manager/modules/neovim/nvim/lua/plugins/ddc/init.lua
@@ -3,10 +3,32 @@ return {
     "Shougo/ddc.vim",
     name = "ddc",
     dependencies = {
+      -- runtime
       "denops",
-      "denops-signature_help",
+
+      -- sources
+      "ddc-source-lsp",
+      "ddc-source-file",
+      "ddc-source-rg",
+
+      -- cmdline sources
+      "ddc-source-cmdline",
+      "ddc-source-cmdline_history",
+      "ddc-source-around",
+      "ddc-source-input",
+      "ddc-source-line",
+
+      -- filters
+      "ddc-filter-matcher_head",
+      "ddc-filter-converter_color",
+
+      -- UI
+      "pum.vim",
+      "ddc-ui-pum",
+
+      -- others
+      "ddc-fuzzy",
     },
-    import = "plugins.ddc",
     init = function()
       local opts = { noremap = true, silent = true, expr = false }
       local keymap = vim.keymap
@@ -35,19 +57,119 @@ return {
       keymap.set("i", "<C-e>", function() vim.fn["pum#map#cancel"]() end, {
         noremap = true,
       })
+
+      function CommandlinePre()
+        -- コマンドラインモード用のキーマッピングを設定
+        vim.keymap.set(
+          "c",
+          "<C-j>",
+          function() vim.fn["pum#map#insert_relative"](1) end
+        )
+        vim.keymap.set(
+          "c",
+          "<C-k>",
+          function() vim.fn["pum#map#insert_relative"](-1) end
+        )
+        vim.keymap.set("c", "<C-y>", function() vim.fn["pum#map#confirm"]() end)
+        vim.keymap.set("c", "<C-e>", function() vim.fn["pum#map#cancel"]() end)
+
+        -- DDCCmdlineLeave イベントで一度だけ CommandlinePost を呼び出す
+        vim.api.nvim_create_autocmd("User", {
+          pattern = "DDCCmdlineLeave",
+          once = true,
+          callback = function() CommandlinePost() end,
+        })
+
+        -- 次のコマンドライン入力のために補完を有効化
+        vim.fn["ddc#enable_cmdline_completion"]()
+      end
+
+      function CommandlinePost()
+        -- コマンドラインモードのキーマッピングを解除
+        pcall(vim.keymap.del, "c", "<C-j>")
+        pcall(vim.keymap.del, "c", "<C-k>")
+        pcall(vim.keymap.del, "c", "<C-y>")
+        pcall(vim.keymap.del, "c", "<C-e>")
+      end
+
+      keymap.set("n", ":", function()
+        CommandlinePre()
+        return ":"
+      end, { expr = true })
     end,
     config = function()
       local helpers = require("helpers.ddc")
 
+      -- UIに使用するpum.vimの設定
+      vim.fn["pum#set_option"]({
+        padding = true,
+        preview = true,
+        border = "rounded",
+      })
+
+      -- ddc.vimのグローバル設定
       helpers.patch_global({
+        -- UIの有効化
         ui = "pum",
+
+        autoCompleteEvents = {
+          "InsertEnter",
+          "TextChangedI",
+          "TextChangedP",
+          "CmdlineEnter",
+          "CmdlineChanged",
+        },
+
+        -- 使用するsourceの定義
         sources = {
           "lsp",
           "rg",
           "file",
         },
+        cmdlineSources = {
+          [":"] = {
+            "cmdline_history",
+            "cmdline",
+            "around",
+          },
+          ["@"] = {
+            "cmdline_history",
+            "cmdline",
+            "file",
+            "around",
+          },
+          [">"] = {
+            "cmdline_history",
+            "cmdline",
+            "file",
+            "around",
+          },
+          ["/"] = {
+            "around",
+            "line",
+          },
+          ["?"] = {
+            "around",
+            "line",
+          },
+          ["-"] = {
+            "around",
+            "line",
+          },
+          ["="] = {
+            "input",
+          },
+        },
+      })
+
+      -- ddc-source-rgの有効化
+      helpers.patch_global({
         sourceOptions = {
-          _ = {
+          rg = {
+            mark = "[RG]",
+            minAutoCompleteLength = 4,
+            maxItems = 5,
+
             matchers = {
               "matcher_fuzzy",
             },
@@ -64,81 +186,9 @@ return {
             hlGroup = "CurSearch",
           },
         },
-        backspaceCompletion = true,
       })
 
-      vim.fn["ddc#enable"]()
-      vim.fn["signature_help#enable"]()
-    end,
-  },
-  {
-    "Shougo/pum.vim",
-    name = "pum.vim",
-    dependencies = {
-      "ddc",
-    },
-    config = function()
-      vim.fn["pum#set_option"]({
-        padding = true,
-        preview = true,
-        border = "rounded",
-      })
-    end,
-  },
-  {
-    "Shougo/ddc-ui-pum",
-    name = "ddc-ui-pum",
-    dependencies = {
-      "ddc",
-    },
-  },
-  {
-    "matsui54/denops-signature_help",
-    name = "denops-signature_help",
-    dependencies = {
-      "denops",
-    },
-    config = function()
-      vim.g["signature_help_config"] = {
-        contentStyle = "remainingLabels",
-        viewStyle = "virtual",
-      }
-    end,
-  },
-  {
-    "tani/ddc-fuzzy",
-    name = "ddc-fuzzy",
-    dependencies = {
-      "ddc",
-    },
-  },
-  {
-    "Shougo/ddc-source-rg",
-    name = "ddc-source-rg",
-    dependencies = {
-      "ddc",
-    },
-    config = function()
-      local helpers = require("helpers.ddc")
-      helpers.patch_global({
-        sourceOptions = {
-          rg = {
-            mark = "[RG]",
-            minAutoCompleteLength = 4,
-            maxItems = 10,
-          },
-        },
-      })
-    end,
-  },
-  {
-    "LumaKernel/ddc-source-file",
-    name = "ddc-source-file",
-    dependencies = {
-      "ddc",
-    },
-    config = function()
-      local helpers = require("helpers.ddc")
+      -- ddc-source-fileの有効化
       helpers.patch_global({
         sourceOptions = {
           file = {
@@ -146,9 +196,179 @@ return {
             isVolatile = true,
             forceCompletionPattern = "\\S/\\S*",
             maxItems = 10,
+            matchers = {
+              "matcher_fuzzy",
+            },
+            sorters = {
+              "sorter_fuzzy",
+            },
+            converters = {
+              "converter_fuzzy",
+            },
           },
         },
       })
+
+      -- ddc-source-lspの有効化
+      helpers.patch_global({
+        sourceOptions = {
+          lsp = {
+            mark = "[LSP]",
+            dup = "keep",
+            keywordPattern = "\\k+",
+            maxItems = 15,
+            matchers = {
+              "matcher_fuzzy",
+            },
+            sorters = {
+              "sorter_fuzzy",
+            },
+            converters = {
+              "converter_color",
+              "converter_fuzzy",
+            },
+          },
+        },
+        sourceParams = {
+          lsp = {
+            lspEngine = "nvim-lsp",
+            snippetEngine = vim.fn["denops#callback#register"](
+              function(body) vim.fn["vsnip#anonymous"](body) end
+            ),
+            enableResolveItem = true,
+            enableAdditionalTextEdit = true,
+            confirmBehavior = "replace",
+          },
+        },
+      })
+
+      -- ddc-source-cmdlineの有効化
+      helpers.patch_global({
+        sourceOptions = {
+          cmdline = {
+            mark = "[CMD]",
+            maxItems = 10,
+            matchers = {
+              "matcher_fuzzy",
+            },
+            sorters = {
+              "sorter_fuzzy",
+            },
+            converters = {
+              "converter_fuzzy",
+            },
+          },
+        },
+      })
+
+      -- ddc-source-cmdline_historyの有効化
+      helpers.patch_global({
+        sourceOptions = {
+          ["cmdline_history"] = {
+            mark = "[HISTORY]",
+            maxItems = 10,
+            matchers = {
+              "matcher_fuzzy",
+            },
+            sorters = {
+              "sorter_fuzzy",
+            },
+            converters = {
+              "converter_fuzzy",
+            },
+          },
+        },
+      })
+
+      -- ddc-source-aroundの有効化
+      helpers.patch_global({
+        sourceOptions = {
+          around = {
+            mark = "[AROUND]",
+            maxItems = 5,
+            matchers = {
+              "matcher_fuzzy",
+            },
+            sorters = {
+              "sorter_fuzzy",
+            },
+            converters = {
+              "converter_fuzzy",
+            },
+          },
+        },
+      })
+
+      -- ddc-source-inputの有効化
+      helpers.patch_global({
+        sourceOptions = {
+          around = {
+            mark = "[INPUT]",
+            maxItems = 5,
+            isVolatile = true,
+            matchers = {
+              "matcher_fuzzy",
+            },
+            sorters = {
+              "sorter_fuzzy",
+            },
+            converters = {
+              "converter_fuzzy",
+            },
+          },
+        },
+      })
+
+      vim.fn["ddc#enable"]()
+      vim.fn["ddc#enable_cmdline_completion"]()
     end,
+  },
+  {
+    "Shougo/pum.vim",
+    name = "pum.vim",
+  },
+  {
+    "Shougo/ddc-ui-pum",
+    name = "ddc-ui-pum",
+  },
+  {
+    "tani/ddc-fuzzy",
+    name = "ddc-fuzzy",
+  },
+  {
+    "haxibami/ddc-filter-converter_color",
+    name = "ddc-filter-converter_color",
+  },
+  {
+    "Shougo/ddc-source-rg",
+    name = "ddc-source-rg",
+  },
+  {
+    "Shougo/ddc-source-lsp",
+    name = "ddc-source-lsp",
+  },
+  {
+    "LumaKernel/ddc-source-file",
+    name = "ddc-source-file",
+  },
+  {
+    "Shougo/ddc-source-cmdline",
+    name = "ddc-source-cmdline",
+  },
+  {
+    "Shougo/ddc-source-cmdline_history",
+    name = "ddc-source-cmdline_history",
+  },
+  {
+    "Shougo/ddc-source-around",
+    name = "ddc-source-around",
+  },
+  {
+    "Shougo/ddc-source-input",
+    name = "ddc-source-input",
+  },
+  {
+    "Shougo/ddc-source-line",
+    name = "ddc-source-line",
   },
 }
--- nix/home-manager/modules/neovim/nvim/lua/plugins/markdown.lua
+++ nix/home-manager/modules/neovim/nvim/lua/plugins/markdown.lua
@@ -4,7 +4,7 @@ return {
     "yousefhadder/markdown-plus.nvim",
     ft = { "markdown" },
     opts = {
-      enabled = true
-    }
+      enabled = true,
+    },
   },
 }
