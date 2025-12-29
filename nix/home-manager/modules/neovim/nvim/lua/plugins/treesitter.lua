7c954193d1f2026de41edcbeaaea202703298ee7
koralle <koralle+github@koralle-mgmg.com>
2025-12-30 00:45:55 +0900

    feat(neovim): update configs of treesitter

--- nix/home-manager/modules/neovim/nvim/lua/plugins/treesitter.lua
+++ nix/home-manager/modules/neovim/nvim/lua/plugins/treesitter.lua
@@ -1,11 +1,28 @@
 ---@type LazySpec
 return {
   {
+    -- context.vimというプラグインの代替。
+    -- treesitterを利用して`context`という概念を持ち込む。
+    -- https://github.com/nvim-treesitter/nvim-treesitter-context
+    -- https://github.com/wellle/context.vim
     "nvim-treesitter/nvim-treesitter-context",
     dependencies = {
       "nvim-treesitter",
     },
-    opts = {},
+    init = function ()
+      local opts = { silent = true }
+
+      vim.keymap.set("n", "[c", function()
+        require("treesitter-context").go_to_context(vim.v.count1)
+      end, opts)
+    end,
+    config = function ()
+      require('treesitter-context').setup({})
+
+      -- コンテキストウィンドウの最終行の部分、ウインドウ全体に広がるように下線を引く
+      vim.api.nvim_set_hl(0, 'TreesitterContextBottom', { underline = true, sp = 'Grey' })
+      vim.api.nvim_set_hl(0, 'TreesitterContextLineNumberBottom', { underline = true, sp = 'Grey' })
+    end
   },
   {
     "nvim-treesitter/nvim-treesitter",
@@ -53,20 +70,19 @@ return {
     },
   },
   {
+    -- https://github.com/JoosepAlviste/nvim-ts-context-commentstring
     "JoosepAlviste/nvim-ts-context-commentstring",
     dependencies = {
       "nvim-treesitter",
     },
-    opts = {},
-  },
-  {
-    "windwp/nvim-ts-autotag",
-    dependencies = {
-      "nvim-treesitter",
+    opts = {
+      -- Normalモードで`gcc`を入力するとcommentstring機能が動く
+      enable_autocmd = true
     },
-    opts = {},
   },
   {
+    -- 関数/メソッド/式などの後に現在のコンテキストに関するvirtual textを表示する
+    -- https://github.com/andersevenrud/nvim_context_vt
     "andersevenrud/nvim_context_vt",
     dependencies = {
       "nvim-treesitter",
